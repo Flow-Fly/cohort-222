f1fa03f12d606cf75d3805f72450fcb163917534
Diego Méndez <diegomendezp@MacBook-Pro-de-Diego.local>
2022-05-15 15:23:03 +0200

    merge changes

--- module-2/week-3/day-3/book-store/data/books.js
+++ module-2/week-3/day-3/book-store/data/books.js
@@ -1,4 +1,4 @@
-module.exports = [
+const books = [
   {
       title: "The Hunger Games",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -110,3 +110,5 @@ module.exports = [
       rating: 8
   }
 ]
+
+module.exports = books;
