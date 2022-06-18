d75f66c72391759d49caa58a9ca913e827c80510
Diego Méndez <diegomendezp@MacBook-Pro-de-Diego.local>
2022-06-18 12:07:21 +0200

    feat: Lists & Conditionals

--- module-3/week1/day-3/react-lists-and-conditional/src/App.js
+++ module-3/week1/day-3/react-lists-and-conditional/src/App.js
@@ -4,27 +4,60 @@ import MovieItem from './components/MovieItem';
 import moviesData from './data'
 
 function App() {
-  const [ movies, setMovies ] = useState(moviesData);
-  
+  const [movies, setMovies] = useState(moviesData);
+
   const handleDelete = (id) => {
     setMovies(movies.filter(movie => movie.id !== id));
   }
+
+  const handleClean = () => {
+    // movies = []; - Change the value directly 🛑
+    setMovies([]); // Use the set function to change the value ✅
+  }
+
+
+  // Bonus
+  // const handleSort = (property) => {...} 
 
   const handleSortByYear = () => {
     const moviesCopy = [...movies]; // Mandatory to create a copy of the array because sort modifies the array
     setMovies(moviesCopy.sort((movie1, movie2) => movie1.year - movie2.year));
   }
 
-  const handleClean = () => {
-    // movies = [];
-    setMovies([]);
+  const handleSortById = () => {
+    const moviesCopy = [...movies];
+    setMovies(moviesCopy.sort((movie1, movie2) => movie1.id - movie2.id))
+  }
+
+  const handleSortByRuntime = () => {
+    const moviesCopy = [...movies];
+    setMovies(moviesCopy.sort((movie1, movie2) => movie1.runtime - movie2.runtime))
+  }
+
+  const handleAdd = () => {
+    const newMovie = { 
+      id: movies.length + 1, 
+      hasOscars: false, 
+      title: "Inception", 
+      year: "2010", 
+      runtime: "148", 
+      genres: ["Science Fiction"], 
+      director: "Christopher Nolan", 
+      actors: "Leonardo DiCaprio, Ken Watanab, Joseph Gordon-Levit, Marion Cotillar, Elliot, Tom Hard, Cillian Murph, Tom Berenge, Michael Caine", 
+      plot: "Cobb and Arthur are 'extractors'; they perform corporate espionage using experimental dream-sharing technology to infiltrate their targets..", 
+      posterUrl: "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg?20170322172828" 
+    }
+    const moviesCopy = [...movies];
+    moviesCopy.push(newMovie);
+    
+    setMovies(moviesCopy);
   }
 
   const displayMovies = () => {
-    // forEach - Iterate over the array but return undefined
-    // map - Iterate over the array and return a new array
+    // forEach - Iterate over the array but return undefined 🛑
+    // map - Iterate over the array and return a new array ✅
     return (
-      movies.map((movie) => { 
+      movies.map((movie) => {
         return (
           <MovieItem
             key={movie.id}
@@ -39,7 +72,7 @@ function App() {
             // actors={movie.actors}
 
             // PRO VERSION - Will pass one prop by each property of the movie object
-            {...movie} 
+            {...movie}
             handleDelete={handleDelete}
           />
         )
@@ -49,7 +82,12 @@ function App() {
   return (
     <div>
       <h1>Movies list App</h1>
-      <button onClick={() => handleSortByYear()}>Sort by year</button>
+      <div className='action-buttons'>
+        <button onClick={() => handleSortByYear()}>Sort by year</button>
+        <button onClick={() => handleSortById()}>Sort by Id</button>
+        <button onClick={() => handleSortByRuntime()}>Sort by Runtime</button>
+        <button onClick={() => handleAdd()}>Add movie</button>
+      </div>
       <div className='movies-list'>
         {
           displayMovies()
--- module-3/week1/day-3/react-lists-and-conditional/src/components/MovieItem.js
+++ module-3/week1/day-3/react-lists-and-conditional/src/components/MovieItem.js
@@ -40,25 +40,36 @@
 
 
 // VERSION 2 - VERSION PRO
-export default function MovieItem({ id, title, director, plot, genres, actors, handleDelete, year }) {
-  return (
-    <div className='movie' >
-      <h3>Title: {title}</h3>
-      <p>Director: {director}</p>
-      <p>Plot: {plot}</p>
-      <p>Actors: {actors}</p>
-      <p>Year: {year}</p>
-      <p>Genres:</p>
-      <ul>
-        {
-          genres.map((genre, index) => {
-            return (
-              <li key={`genre-${index}`}>{index}-{genre}</li>
-            )
-          })
-        }
-      </ul>
-      <button onClick={() => handleDelete(id)}>Delete movie 🗑</button>
-    </div>
-  )
+export default function MovieItem({ id, title, director, plot, genres, actors, handleDelete, year, runtime, posterUrl, hasOscars }) {
+  if(false){
+    return (
+      <p>Loading...</p>
+    )
+  } else {
+    return (
+      <div className='movie' >
+        <img src={posterUrl} alt="no-poster" />
+        <p>Id: {id}</p>
+        <h3>Title: {title}</h3>
+        <p>Director: {director}</p>
+        <p>Plot: {plot}</p>
+        <p>Actors: {actors}</p>
+        <p>Year: {year}</p>
+        <p>Runtime: {runtime}</p>
+        <p>Genres:</p>
+        {/* {hasOscars ? 'Oscars awarded 🙌' : 'Not Oscars awarded 💆‍♂️'} */}
+        {hasOscars && <p>Oscars awarded 🙌</p>}
+        <ul>
+          {
+            genres.map((genre, index) => {
+              return (
+                <li key={`genre-${index}`}>{index}-{genre}</li>
+              )
+            })
+          }
+        </ul>
+        <button onClick={() => handleDelete(id)}>Delete movie 🗑</button>
+      </div>
+    )
+  } 
 }
